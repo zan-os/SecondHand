23c895438704bc4b6913a750549f2b47d3334679
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-06-17 00:42:21 +0700

    Add Update Profile Fragment

--- app/src/main/java/id/co/secondhand/ui/market/profile/update/UpdateProfileFragment.kt
+++ app/src/main/java/id/co/secondhand/ui/market/profile/update/UpdateProfileFragment.kt
@@ -1,4 +1,4 @@
-package id.co.secondhand.ui.market.product.add
+package id.co.secondhand.ui.market.profile.update
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import id.co.secondhand.R
 
-class AddProductFragment : Fragment() {
+class UpdateProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_product, container, false)
+        return inflater.inflate(R.layout.fragment_update_profile, container, false)
     }
 }
