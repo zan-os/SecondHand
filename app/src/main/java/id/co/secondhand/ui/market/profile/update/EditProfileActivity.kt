b78e48710549905ec6f5082d601208766ee25561
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-06-21 20:48:20 +0700

    Update UI EditProfile

--- app/src/main/java/id/co/secondhand/ui/market/profile/update/EditProfileActivity.kt
+++ app/src/main/java/id/co/secondhand/ui/market/profile/update/EditProfileActivity.kt
@@ -1,7 +1,12 @@
 package id.co.secondhand.ui.market.profile.update
 
 import android.os.Bundle
+import android.text.InputType
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import gun0912.tedimagepicker.builder.TedImagePicker
+import id.co.secondhand.R
 import id.co.secondhand.databinding.ActivityEditProfileBinding
 
 class EditProfileActivity : AppCompatActivity() {
@@ -12,5 +17,38 @@ class EditProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        saveUser()
+    }
+
+    private fun saveUser() {
+        validateEdit()
+    }
+
+    private fun validateEdit() {
+        imagePicker()
+        dropDownMenu()
+        binding.addressEt.setRawInputType(InputType.TYPE_CLASS_TEXT)
+
+    }
+
+    private fun imagePicker() {
+        binding.profileImageContainer.setOnClickListener {
+            TedImagePicker.with(this)
+                .start { uri ->
+                    Glide.with(this)
+                        .load(uri)
+                        .override(300)
+                        .centerCrop()
+                        .into(binding.photoProfileIv)
+                }
+            window.decorView.clearFocus()
+        }
+    }
+
+    private fun dropDownMenu() {
+        val city = resources.getStringArray(R.array.city)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_city_item, city)
+        binding.autoCompleteCityTv.setAdapter(arrayAdapter)
     }
 }
