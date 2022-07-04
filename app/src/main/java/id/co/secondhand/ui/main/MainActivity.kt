aa6ea60a0045b6387703b6ba3d85d4e5185069de
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-07-04 23:14:23 +0700

    add notification status on BottomNavigationView

--- app/src/main/java/id/co/secondhand/ui/main/MainActivity.kt
+++ app/src/main/java/id/co/secondhand/ui/main/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import id.co.secondhand.R
+import id.co.secondhand.data.resource.Resource
 import id.co.secondhand.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
@@ -22,9 +23,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getAccessToken()
+        setupNavView()
+    }
+
+    private fun getAccessToken() {
+        viewModel.token.observe(this) { token ->
+            getNotification(token)
+        }
+    }
+
+    private fun getNotification(token: String) {
+        viewModel.getNotification(token).observe(this) { result ->
+            when (result) {
+                is Resource.Loading -> {}
+                is Resource.Success -> {
+                    result.data?.map { notification ->
+                        binding.navView.getOrCreateBadge(R.id.sellerNotificationFragment)
+                            .isVisible = !notification.read
+                    }
+                }
+                is Resource.Error -> {}
+            }
+        }
+    }
+
+    private fun setupNavView() {
         val navController = findNavController(R.id.nav_host_fragment)
         binding.navView.setupWithNavController(navController)
-        binding.navView.getOrCreateBadge(R.id.sellerNotificationFragment).isVisible = true
     }
 }
 
