044fa204c88604b02865f22ce6dbbedb2c7432ca
Fawaz Abdurrahman <fawazabdurrahman130901@gmail.com>
2022-07-07 05:48:14 +0700

    layout seller 25

---
+++ app/src/main/java/id/co/secondhand/ui/market/product/negotiate/BidderInfoActivity.kt
@@ -0,0 +1,12 @@
+package id.co.secondhand.ui.market.product.negotiate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.secondhand.R
+
+class BidderInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bidder_info)
+    }
+}
