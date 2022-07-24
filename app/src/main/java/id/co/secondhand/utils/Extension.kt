a878558709357ec95d9f06c3fee00313690093a1
Fauzan Osama <osamafauzan33@gmail.com>
2022-07-24 20:24:50 +0700

    fixing missing import

--- app/src/main/java/id/co/secondhand/utils/Extension.kt
+++ app/src/main/java/id/co/secondhand/utils/Extension.kt
@@ -14,6 +14,7 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
 import java.text.NumberFormat
+import java.text.SimpleDateFormat
 import java.util.*
 
 object Extension {
