df3445d1685102f033f21cf1f60b9a979a122a8d
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-07-25 11:56:58 +0700

    Update layout negotiate

--- app/src/main/java/id/co/secondhand/ui/market/product/detail/NegotiateFragment.kt
+++ app/src/main/java/id/co/secondhand/ui/market/product/detail/NegotiateFragment.kt
@@ -1,7 +1,6 @@
 package id.co.secondhand.ui.market.product.detail
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -67,7 +66,6 @@ class NegotiateFragment : BottomSheetDialogFragment() {
             if (token.isEmpty()) {
                 binding.sendNegotiateBtn.text = getString(R.string.silahkan_login)
             } else {
-                binding.sendNegotiateBtn.isEnabled = true
                 bidProduct(token, productId)
             }
         }
